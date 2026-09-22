a680877d3e66d4f793bdeb5c18337a1314b0aabd
ljlazada11 <ljlazada11@gmail.com>
2026-09-22 09:25:41 +0800

    final

---
+++ ACTIVITY 5/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('./config/config.php');
+require_once('./helpers/alert_helper.php');
+
+// import your Controllers here..
+require_once('./controller/MainController.php');
+
+// create an instance for you Controller
+$main = new MainController();
+
+require_once('./router.php');
+
